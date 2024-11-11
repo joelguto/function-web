8905e15c7829fc836c4d0f5ba4b50d81b7a15eb5
joelguto <joelguto715@gmail.com>
2024-11-11 09:56:11 +0300

    Initial push to repo

---
+++ src/test/java/com/guto/functionweb/FunctionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guto.functionweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
